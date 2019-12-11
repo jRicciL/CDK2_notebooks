f955e2ec3d2428a533f81e7c03520d27fc0afab0
Joel Ricci-Lopez <ricci.lob@gmail.com>
2019-12-11 15:58:42 -0800

    Function to plot MDA, MDS per md phase, and RMSD

--- 4_Trajectory_Analysis/miscellanious_traj_functions.py
+++ 4_Trajectory_Analysis/miscellanious_traj_functions.py
@@ -1,5 +1,8 @@
 from modules.MDS import cMDS_proj
 from sklearn.decomposition import PCA
+import numpy as np
+import pandas as pd
+import pytraj as pyt
 
 def process_pca(traj, ref = 0,  n_vecs = 2):
     '''
@@ -67,3 +70,56 @@ def get_sup_points_projected_mds(MDS_ref_object, traj_reference,
     mds_projected = cMDS_proj(MDS_ref_object, mtx_rmsd_sup.T)
     
     return( mds_projected[: , 0 : n_components].T )
+
+import matplotlib.pyplot as plt
+def example_traj_projection(Z , mds_pisani_402,
+                            sistema, df_prot, rmsd_full_traj,
+                            dic_n_frames_by_phase, plot_points,
+                            plot_anotation_labels):
+    # Se genera la gráfica de la proyección
+    # sistema = '4fku'
+    fig = plt.figure(figsize=(16, 16))
+    # Proyección de referencia
+    plt.subplot(2, 2, 1)
+    plot_mds_ref_args = dict(mds = mds_pisani_402[0], colors_list = ['grey'], alpha = 0.2, point_size = 100)
+    plot_points(**plot_mds_ref_args, title = "cMDS: Proyección de puntos sumplementarios")
+
+    # Proyección de los puntos "out of sample"
+    # Trayectoria completa
+    #Z = full_traj_4fku_projection
+    plt.scatter(Z[0], Z[1], marker = 'D', c='#FB7070', s = 30, alpha = 0.3)
+    plot_anotation_labels(list_of_all_index = df_prot.index,  list_of_ref_names = [sistema], 
+                          x = mds_pisani_402[0][0], y = mds_pisani_402[0][1], 
+                          xytext_delta=[0.4, 0.3], s=130);
+
+    # Proyección por fases
+    ax = plt.subplot(2, 2, 2)
+    alpha = 0.3
+    ax.grid()
+    # proyección de cristal y minimización
+    plot_points(**plot_mds_ref_args, title = "cMDS: Trayectoria por Fases")
+    colors = ['red', 'green', '#F78417', '#BF71F9', '#3776E1']
+    for phase, f_range, color in \
+        zip(dic_n_frames_by_phase.keys(), dic_n_frames_by_phase.values(), colors):
+        ax.scatter(Z[0][f_range], Z[1][f_range], marker = 'D', s =  60, alpha = alpha,
+               label = phase.upper(), c = color)
+    plot_anotation_labels(list_of_all_index = df_prot.index,  list_of_ref_names = [sistema], 
+                      x = mds_pisani_402[0][0], y = mds_pisani_402[0][1], 
+                      xytext_delta=[0.2, 0.2], s=130);
+    ax.legend()
+    padding = 0.2
+    lims_x = (Z[0].min() - padding, Z[0].max() + padding)
+    lims_y = (Z[1].min() - padding, Z[1].max() + padding)
+    plt.xlim(lims_x)
+    plt.ylim(lims_y);
+
+    # Gráfica del RMSD
+    plt.subplot(2, 1, 2)
+    for phase, f_range, color in \
+        zip(dic_n_frames_by_phase.keys(), dic_n_frames_by_phase.values(), colors):
+        plt.scatter(x = f_range,  y = rmsd_full_traj[f_range], marker = 'D', s =  60, 
+                   alpha = 0.5, label = phase.upper(), c = color)
+    plt.title(F'{sistema.upper()}: RMSD de la trayectoria completa')
+    plt.xlabel('# Frames'); plt.ylabel('RMSD (A)')
+    plt.legend()
+    plt.grid()
