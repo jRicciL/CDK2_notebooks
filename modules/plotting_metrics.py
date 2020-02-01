58ba2968ea1c11496303d5298487175a0cbbcce3
Joel Ricci-Lopez <ricci.lob@gmail.com>
2020-01-31 16:20:16 -0800

    BEDROC and RIE metrics included

--- modules/plotting_metrics.py
+++ modules/plotting_metrics.py
@@ -4,6 +4,8 @@ import pandas as pd
 import seaborn as sns 
 import matplotlib.pyplot as plt
 import pylab
+import warnings
+
 
 
 # Metric imported from other libraries
@@ -33,7 +35,9 @@ class PlotMetric:
                                   'auac': self._get_ac_auc,
                                   'pr_auc': self._get_pr_auc,
                                   'ref_auc': self._get_ref_auc,
-                                  'ef': self.get_efs}
+                                  'ef': self.get_efs,
+                                  'rie': self._rie_score,
+                                  'bedroc': self._bedroc_score}
         pylab.rcParams['figure.figsize'] = figsize
         sns.set( context = 'talk', style = 'white', palette = color_palette)
     
@@ -168,7 +172,7 @@ class PlotMetric:
         df_efs = pd.DataFrame(ef_results, index = row_names_ef)
         df_efs["#ligs at X%"] = [np.floor(i* self.N) for i in fractions]
         df_efs = df_efs.round(rounded)
-        return df_efs
+        return df_efs.T
 
     def _get_ref_auc(self, y_pred, method, max_chi = 1):
         fractions = np.linspace(0.0, max_chi, len(self.y_true) - 2 )
@@ -176,6 +180,61 @@ class PlotMetric:
         efs_auc = auc(fractions, efs)
         return efs_auc
 
+    def _rie_score(self, y_pred, alpha):
+        N = self.N
+        n = self.n
+        y_true = self.y_true
+        
+        warnings.simplefilter('always', UserWarning)
+        if alpha*self.R_a >= 1:
+            max_alpha = 1/self.R_a
+            warnings.warn('Parsed alpha value ({:0.2f}) times R_a is greater than 1.\nAn alpha value below {:0.2f} is recommended.'.format(alpha, max_alpha),
+            ResourceWarning)
+        order = np.argsort(- y_pred)
+        m_rank = (y_true[order] == 1).nonzero()[0] + 1
+        S_numerator = np.sum(np.exp(-alpha * m_rank / N))
+        _denominator =  (n / N) * ( (1 - np.exp(-alpha)) / (np.exp(alpha/N) - 1) )
+        
+        RIE = S_numerator / _denominator
+        return RIE
+
+    def _rie_max_min_scores(self, alpha):
+        R_a = self.R_a
+        RIE_min = (1 - np.exp(alpha * R_a)) / (R_a*(1 - np.exp(alpha)))
+        RIE_max = (1 - np.exp(- alpha * R_a)) / (R_a*(1 - np.exp(- alpha)))
+        return (RIE_min, RIE_max)
+
+    def _bedroc_score(self, y_pred, alpha):
+        N = self.N
+        n = self.n
+        y_true = self.y_true
+        R_a = self.R_a
+        
+        RIE = self._rie_score(y_pred, alpha)
+        RIE_min, RIE_max = self._rie_max_min_scores(alpha)
+        
+        BEDROC = (RIE - RIE_min) / (RIE_max - RIE_min)
+        return BEDROC
+
+    def get_ries(self, alphas = [5, 10, 20], rounded = 3):
+        rie_results = {}
+        for key, y_pred in self.y_pred_dict.items():
+            rie_results[key] = [self._rie_score(y_pred = y_pred, alpha = a) for a in alphas]
+        row_names_rie = [F'alpha = {a}' for a in alphas]
+        df_rie = pd.DataFrame(rie_results, index = row_names_rie)
+        df_rie = df_rie.round(rounded).T
+        return df_rie
+
+    def get_bedrocs(self, alphas = [5, 10, 20], rounded = 3):
+        bedrocs_results = {}
+        for key, y_pred in self.y_pred_dict.items():
+            bedrocs_results[key] = [self._bedroc_score(y_pred = y_pred, alpha = a) for a in alphas]
+        row_names_bedroc = [F'alpha = {a}' for a in alphas]
+        df_bedrocs = pd.DataFrame(bedrocs_results, index = row_names_bedroc)
+        df_bedrocs = df_bedrocs.round(rounded).T
+        return df_bedrocs
+
+    ## PLOTTING FUNCTIONS
     def _add_plot_ef(self, y_pred, label, max_chi, max_num_of_ligands, method = 'normalized',  **kwargs):
         fractions = np.linspace(0.0, 1, len(self.y_true) - 2 )
         names = {'relative': 'REF', 'absolute': 'EF', 'normalized': 'NEF'}
@@ -309,6 +368,35 @@ class PlotMetric:
             if show_by_itself:
                 plt.show()
 
+    def _add_plot_bedroc(self, label, alphas, bedrocs, **kwargs):
+        plt.plot(alphas, bedrocs, label = label,  **kwargs)
+
+    def plot_bedroc(self, title, alphas, keys_to_omit = [], key_to_plot = None, key_to_fade = None,
+                     fontsize='small', showplot = True, show_by_itself=True, **kwargs):
+        sns.color_palette(self.color_palette)
+        df_bedroc = self.get_bedrocs(alphas = alphas).T
+        for key in self.y_pred_dict.keys():
+            if key in keys_to_omit:
+                continue
+            if key_to_fade == key:
+                self._add_plot_bedroc(label = key, alphas = alphas, linestyle = '--', marker = 'o',
+                bedrocs = df_bedroc[key].values, **kwargs)
+                continue
+            if type(key_to_plot) is str and key_to_plot in self.y_pred_dict.keys():
+                key = key_to_plot
+                self._add_plot_bedroc(label = key, alphas = alphas, bedrocs = df_bedroc[key].values, marker = 'v', **kwargs)
+                break
+            self._add_plot_bedroc(label = key, alphas = alphas, bedrocs = df_bedroc[key].values, marker = 'v', **kwargs)
+        if showplot:
+            plt.legend(fontsize=fontsize)
+            plt.xlabel("Alpha values")
+            plt.ylabel("BEDROC score")
+            plt.grid(linestyle='--', linewidth='0.8')
+            plt.title(title)
+            plt.ylim(-0.1, 1.1)
+            if show_by_itself:
+                plt.show()
+
     # Plotting functions
     def _add_plot_roc(self, y_pred, label, **kwargs):
         fpr, tpr = self._get_roc(y_pred)
@@ -372,7 +460,7 @@ class PlotMetric:
             else:
                 x_label = 'Ranking positions (k)'
                 y_label = 'Num. of actives (Fa(k))'
-            plt.plot([0, 1], [1, 1], 'k--', c = 'grey')
+            plt.plot([0, 0], [1, 1], 'k--', c = 'grey')
             plt.xlabel(x_label)
             plt.ylabel(y_label)
             plt.grid(linestyle='--', linewidth='0.8')
