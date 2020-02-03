b6868fd0656d469d7c810c4c689c7933a97444ee
Joel Ricci-Lopez <ricci.lob@gmail.com>
2020-02-03 11:46:24 -0800

    Last updates

--- 6_Machine_Learning_Models/auxiliar_plot_swarm.py
+++ 6_Machine_Learning_Models/auxiliar_plot_swarm.py
@@ -2,8 +2,29 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 import pandas as pd
 import numpy as np
+import holoviews as hv
+from holoviews import dim
+from bokeh.io import show
+
 cmp = sns.set_palette('Dark2')
 sns.set(context = "talk", font_scale=0.9, style = 'white', palette = cmp)
+
+# Plot with holoviews
+def boxplot_metrics(data, title, color_by = 'Method'):
+    data = data.melt(var_name='Variables', value_name='Metric Value')
+    data['Method'] = data['Variables'].apply(lambda x: x.split('-')[0])
+    data['Ranking'] = data['Variables'].apply(lambda x: x.split('-')[1])
+    data['Metric'] = data['Variables'].apply(lambda x: x.split('-')[2])
+    boxwhisker = hv.BoxWhisker(data, ['Metric', 'Ranking', 'Method' ], 
+                               'Metric Value', label = title)
+    boxwhisker.opts(show_legend = False, width=900, height = 500, 
+                    box_fill_color = color_by, cmap='Set3', ylim=(0, 1),
+                    xrotation=45, invert_xaxis = True, toolbar='above')
+    f = hv.render(boxwhisker, backend='bokeh')
+    f.toolbar.logo = None
+    f.grid.grid_line_color = 'lightgrey'
+    f.grid.grid_line_dash = [5, 3]
+    return f
 
 def plot_swarm_metrics(data, title, filter_regex, hue = None, hue_name = None, ylim = (0.3, 1),
                       cmap = 'Dark2', legend = True, **kwargs):
@@ -24,7 +45,7 @@ def plot_swarm_pair(df, title, col = ['DkS', 'DkLEff'], metric='ROC', **kwargs):
     metric = '-' + metric
     plt.subplot(1,2,1)
     plot_swarm_metrics(data = df.filter(regex=col[0], axis = 1), 
-                  title = F'{title}: {col[0]} - {metric}', filter_regex = metric, **kwargs);
+                  title = F'{title}: {col[0]}{metric}', filter_regex = metric, **kwargs);
     plt.subplot(1,2,2)
     plot_swarm_metrics(data = df.filter(regex=col[1], axis=1), 
-                  title = F'{title}: {col[1]} - {metric}', filter_regex = metric, **kwargs);
+                  title = F'{title}: {col[1]}{metric}', filter_regex = metric, **kwargs);
